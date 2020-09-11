3c9b93886f4ad9a5364ef19f551b2525b23f35c7
taehyunje <jetaehyun0@gmail.com>
2020-09-10 21:11:18 -0400

    fixed bugs and edited data packet structure

--- HAB_GCS/src/Data_Packet/sensor_data.c
+++ HAB_GCS/src/Data_Packet/sensor_data.c
@@ -1,1 +1,25 @@
 #include "sensor_data.h"
+
+void unpack_payload(uint8_t* payload) {
+
+}
+
+void sensor_payload_pack(struct sensor_data_t *payload) {
+
+}
+
+void release_data_pack(struct release_data_t *payload) {
+
+}
+
+bool checkPayload() {
+
+}
+
+int calculate_checksum() {
+
+}
+
+bool confirm_checksum() {
+
+}
--- HAB_GCS/src/Data_Packet/sensor_data.h
+++ HAB_GCS/src/Data_Packet/sensor_data.h
@@ -5,23 +5,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-struct sensor_data_t {
+// will send 30 bytes
+struct HAB_payload_t {
     // 0
     uint8_t payload_type;
     // 1
-    float baro_sensor; 
-    // 5
-    float NO2_sensor;
-    // 9
-    float temp_sensor;
-    // 13
-    float UV_sensor;
-    // 17
-    float CO2_sensor;
-    // 21
-    float Ozone_sensor;
-    // 25
-}
+    uint8_t *payload;
+    // 2
+    uint8_t checksum;
+    // 3
+};
+
+struct sensor_data_t {
+    // 0
+    int32_t baro_sensor; 
+    // 4
+    int32_t NO2_sensor;
+    // 8
+    int32_t temp_sensor;
+    // 12
+    int32_t UV_sensor;
+    // 16
+    int32_t CO2_sensor;
+    // 20
+    int32_t Ozone_sensor;
+    // 24
+};
 
 struct release_data_t {
     // 0
@@ -36,9 +45,11 @@ enum payload_type {
     release_payload = 1
 };
 
-void unpack_payload(byte* payload);
+void unpack_payload(uint8_t* payload);
 void sensor_payload_pack(struct sensor_data_t *payload);
 void release_data_pack(struct release_data_t *payload);
 bool checkPayload();
+int calculate_checksum();
+bool confirm_checksum();
 
 #endif
