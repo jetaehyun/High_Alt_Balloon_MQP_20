c273135f9718bb8170c087616239c2255a923c5c
taehyunje <jetaehyun0@gmail.com>
2020-09-20 19:25:01 -0400

    added library changes to weather balloon code

--- HAB_Payload/src/Data_Packet/header/sensor_payload.h
+++ HAB_Payload/src/Data_Packet/header/sensor_payload.h
@@ -8,7 +8,7 @@
 
 struct sensor_data_t {
     // 0
-    int32_t baro_sensor; 
+    int32_t pressure_sensor; 
     // 4
     int32_t NO2_sensor;
     // 8
@@ -20,10 +20,12 @@ struct sensor_data_t {
     // 20
     int32_t Ozone_sensor;
     // 24
+    int32_t altitude;
+    // 28
 };
 
 struct sensor_data_t sensor_payload_unpack(uint8_t *payload);
 uint8_t sensor_payload_pack(uint8_t *payload, struct sensor_data_t *data);
-struct sensor_data_t *create_sensor_payload(int32_t baro_sensor, int32_t NO2_sensor, int32_t temp_sensor, int32_t UV_sensor, int32_t CO2_sensor, int32_t Ozone_sensor);
+struct sensor_data_t *create_sensor_payload(int32_t pressure_sensor, int32_t NO2_sensor, int32_t temp_sensor, int32_t UV_sensor, int32_t CO2_sensor, int32_t Ozone_sensor, int32_t altitude);
 
 #endif
--- HAB_Payload/src/Data_Packet/sensor_payload.c
+++ HAB_Payload/src/Data_Packet/sensor_payload.c
@@ -11,15 +11,16 @@
  * @param Ozone_sensor 
  * @return struct sensor_data_t* 
  */
-struct sensor_data_t *create_sensor_payload(int32_t baro_sensor, int32_t NO2_sensor, int32_t temp_sensor, int32_t UV_sensor, int32_t CO2_sensor, int32_t Ozone_sensor) {
+struct sensor_data_t *create_sensor_payload(int32_t pressure_sensor, int32_t NO2_sensor, int32_t temp_sensor, int32_t UV_sensor, int32_t CO2_sensor, int32_t Ozone_sensor, int32_t altitude) {
     struct sensor_data_t *data = malloc(sizeof(struct sensor_data_t));
 
-    data->baro_sensor = baro_sensor;
+    data->pressure_sensor = pressure_sensor;
     data->NO2_sensor = NO2_sensor;
     data->temp_sensor = temp_sensor;
     data->UV_sensor = UV_sensor;
     data->CO2_sensor = CO2_sensor;
     data->Ozone_sensor = Ozone_sensor;
+    data->altitude = altitude;
 
     return data;
 }
@@ -33,12 +34,13 @@ struct sensor_data_t *create_sensor_payload(int32_t baro_sensor, int32_t NO2_sen
 struct sensor_data_t sensor_payload_unpack(uint8_t* payload) {
     struct sensor_data_t data;
 
-    data.baro_sensor = unpack_32(payload);
+    data.pressure_sensor = unpack_32(payload);
     data.NO2_sensor = unpack_32(payload + 4);
     data.temp_sensor = unpack_32(payload + 8);
     data.UV_sensor = unpack_32(payload + 12);
     data.CO2_sensor = unpack_32(payload + 16);
     data.Ozone_sensor = unpack_32(payload + 20);
+    data.altitude = unpack_32(payload + 24);
  
     return data;
 }
@@ -52,12 +54,13 @@ struct sensor_data_t sensor_payload_unpack(uint8_t* payload) {
  */
 uint8_t sensor_payload_pack(uint8_t *payload, struct sensor_data_t *data) {
 
-    pack_32(payload, data->baro_sensor);
+    pack_32(payload, data->pressure_sensor);
     pack_32(payload + 4, data->NO2_sensor);
     pack_32(payload + 8, data->temp_sensor);
     pack_32(payload + 12, data->UV_sensor);
     pack_32(payload + 16, data->CO2_sensor);
     pack_32(payload + 20, data->Ozone_sensor);
+    pack_32(payload + 24, data->altitude);
 
     return 24;
 
