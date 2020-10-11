1dd2ab5e881efe213d5a714da77869608956941a
taehyunje <jetaehyun0@gmail.com>
2020-10-10 21:22:15 -0400

    flipped server and client

--- HAB_GCS/src/main.c
+++ HAB_GCS/src/main.c
@@ -19,41 +19,31 @@
 #define SIZE 30
 
 int main(int argc, const char* argv[]) {
-
-    int sockfd;
-    char buffer[SIZE] = {'\0'}; 
-    struct sockaddr_in servaddr; 
-    socklen_t len = sizeof(servaddr);
-
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
+    char buffer[100]; 
+    char *message = "Hello Client"; 
+    int listenfd, len; 
+    struct sockaddr_in servaddr, cliaddr; 
+    bzero(&servaddr, sizeof(servaddr)); 
   
-    memset(&servaddr, '\0', sizeof(servaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
+    // Create a UDP Socket 
+    listenfd = socket(AF_INET, SOCK_DGRAM, 0);         
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
     servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_family = AF_INET;  
+   
+    // bind server address to socket descriptor 
+    bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
+       
 
     while(1) {
 
-        int size = recvfrom(sockfd, buffer, SIZE, 0, (struct sockaddr *) &servaddr, &len);
-        
-        printf("HE\n");
-        if(size > 0) {
-
-            printf("%s\n", buffer);
+        if(recvfrom(listenfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL) > 0) {
+            puts(buffer);   
         }
 
-
-        sleep(1);
+        sleep(2);      
     }  
-      
-  
-    close(sockfd); 
+
 
     // if(argc < 1) {
     //     printf("Enter serial port... Ex: /dev/ttyS4\n");
--- HAB_Payload/src/main.c
+++ HAB_Payload/src/main.c
@@ -12,43 +12,37 @@
 #define SIZE 30
 
 int main(int argc, const char *argv[]) {
-    int sockfd; 
-    char *hello = "Hello from server"; 
-    struct sockaddr_in servaddr, cliaddr;
-    socklen_t len = sizeof(cliaddr); 
-      
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    memset(&servaddr, '\0', sizeof(servaddr)); 
-    memset(&cliaddr, '\0', sizeof(cliaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family = AF_INET; // IPv4 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    char buffer[100]; 
+    int sockfd, n; 
+    struct sockaddr_in servaddr; 
+    char *message = "Hello Client"; 
+  
+    // clear servaddr 
+    bzero(&servaddr, sizeof(servaddr)); 
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
     servaddr.sin_port = htons(PORT); 
+    servaddr.sin_family = AF_INET; 
       
-    // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
-            sizeof(servaddr)) < 0 ) 
+    // create datagram socket 
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
+      
+    // connect to server 
+    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
     { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
+        printf("\n Error : Connect Failed \n"); 
+        exit(0); 
     } 
-      
+  
+
     while(1) {
-        printf("write\n");
-
-        sendto(sockfd, hello, SIZE, 0, (struct sockaddr*)&cliaddr, len);
-
-        sleep(1);
+        sendto(sockfd, message, SIZE, 0, 
+          (struct sockaddr*)&servaddr, sizeof(servaddr)); 
+        sleep(2);
     }
 
     close(sockfd); 
-   
+
+           
 
     return 0;
 }
