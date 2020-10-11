d235150e2c31e4895e87514712d225c89bf360ec
taehyunje <jetaehyun0@gmail.com>
2020-10-11 17:21:52 -0400

    udp change1

--- HAB_Payload/src/main.c
+++ HAB_Payload/src/main.c
@@ -6,36 +6,29 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h>
+#include <netdb.h>
 // #include <wiringPi.h>
 
-#define PORT 8808
+#define PORT 1160
 #define SIZE 30
 
 int main(int argc, const char *argv[]) {
-    char buffer[100]; 
-    int sockfd, n; 
+    char buffer[SIZE]; 
+    int sockfd, n;
     struct sockaddr_in servaddr; 
     char *message = "Hello Client"; 
   
-    // clear servaddr 
-    bzero(&servaddr, sizeof(servaddr)); 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_family = AF_INET; 
-      
-    // create datagram socket 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
-      
-    // connect to server 
-    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
-    { 
-        printf("\n Error : Connect Failed \n"); 
-        exit(0); 
-    } 
-  
+      // create datagram socket 
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    memset((char*)&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    // servaddr.sin_port = inet_addr("192.168.0.188"); 
+    servaddr.sin_port = INADDR_ANY; 
 
     while(1) {
-        sendto(sockfd, message, SIZE, 0, 
+        sendto(sockfd, message, strlen(message), 0, 
           (struct sockaddr*)&servaddr, sizeof(servaddr)); 
         sleep(2);
     }
