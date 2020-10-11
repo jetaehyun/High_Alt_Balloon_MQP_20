8e70abb61d74924b354bb2360920e440a011e79f
taehyunje <jetaehyun0@gmail.com>
2020-10-11 18:40:09 -0400

    udp2 server

--- HAB_GCS/src/main.c
+++ HAB_GCS/src/main.c
@@ -16,55 +16,101 @@
 #include "Database/database.h"
 
 #define PORT 1160
-#define SIZE 30
+#define MAXLINE 1024 
 
 //server
 int main(int argc, const char* argv[]) {
 
-    char buffer[SIZE] = {'\0'}; 
-    char *message = "Hello Client"; 
-    int listenfd, len; 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char *hello = "Hello from server"; 
     struct sockaddr_in servaddr, cliaddr; 
-  
-    // Create a UDP Socket 
-    if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Couldn't create socket\n");
-        return 0;
+      
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+      
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = inet_addr("192.168.0.156"); 
+    servaddr.sin_port = htons(PORT); 
+      
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
     }
 
-    memset((char *)&servaddr, 0, sizeof(servaddr));
+    cliaddr.sin_family    = AF_INET; // IPv4 
+    cliaddr.sin_addr.s_addr = INADDR_ANY; 
+    cliaddr.sin_port = htons(PORT);  
+      
+    int len, n; 
+  
+    len = sizeof(cliaddr);  //len is value/resuslt 
+  
+    while(1) {
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                    &len); 
+        buffer[n] = '\0'; 
+        printf("buffer: %s\n", buffer);
+        sleep(2);
+    }
 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    // servaddr.sin_addr.s_addr = inet_addr("192.168.0.188"); 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_family = AF_INET; 
 
-    // cliaddr.sin_addr.s_addr = inet_addr("192.168.0.156"); 
-    // cliaddr.sin_port = htons(PORT); 
-    // cliaddr.sin_family = AF_INET; 
+
+    // char buffer[SIZE] = {'\0'}; 
+    // char *message = "Hello Client"; 
+    // int listenfd, len; 
+    // struct sockaddr_in servaddr, cliaddr; 
+  
+    // // Create a UDP Socket 
+    // if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    //     perror("Couldn't create socket\n");
+    //     return 0;
+    // }
+
+    // memset((char *)&servaddr, 0, sizeof(servaddr));
+
+    // servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    // // servaddr.sin_addr.s_addr = inet_addr("192.168.0.188"); 
+    // servaddr.sin_port = htons(PORT); 
+    // servaddr.sin_family = AF_INET; 
+
+    // // cliaddr.sin_addr.s_addr = inet_addr("192.168.0.188"); 
+    // // cliaddr.sin_port = htons(PORT); 
+    // // cliaddr.sin_family = AF_INET; 
  
    
-    // bind server address to socket descriptor 
-    if(bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-        perror("Failed to bind socket");
-        exit(EXIT_FAILURE);
-    } 
+    // // bind server address to socket descriptor 
+    // if(bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+    //     perror("Failed to bind socket");
+    //     exit(EXIT_FAILURE);
+    // } 
        
 
-    while(1) {
+    // while(1) {
     
-        int some = recvfrom(listenfd, buffer, SIZE, 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
-        if(some > 0) {
-            buffer[some] = '\0';
-            printf("%d\n", some);
-        }
-        // puts(some);   
+    //     int some = recvfrom(listenfd, buffer, SIZE, 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+    //     if(some > 0) {
+    //         buffer[some] = '\0';
+    //         printf("%d\n", some);
+    //     }
+    //     // puts(some);   
         
 
-        sleep(2);      
-    }  
+    //     sleep(2);      
+    // }  
 
-    close(listenfd);
+    // close(listenfd);
 
 
     // if(argc < 1) {
