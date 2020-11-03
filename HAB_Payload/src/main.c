32db3b838b1fd2905ecce8b04ce9d6658bb50cc6
jetaehyun <jetaehyun0@gmail.com>
2020-11-03 15:43:28 -0500

    updated udp code

--- HAB_Payload/src/main.c
+++ HAB_Payload/src/main.c
@@ -8,15 +8,19 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+#include "Data_Packet/header/data_packet.h"
+#include "Data_Packet/header/release_payload.h"
+#include "Data_Packet/header/sensor_payload.h"
+// #include "Database/database.h"
+
 // #include <wiringPi.h>
 
-#define PORT 1160
-#define MAXLINE 1024 
+#define PORT 8808
+#define SIZE 30
 
 int main(int argc, const char *argv[]) {
-int sockfd; 
-    char buffer[MAXLINE]; 
-    char *hello = "Hello from client"; 
+    int sockfd; 
+
     struct sockaddr_in     servaddr; 
   
     // Creating socket file descriptor 
@@ -30,15 +34,28 @@ int sockfd;
     // Filling server information 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = inet_addr("192.168.0.156"); 
+    servaddr.sin_addr.s_addr = inet_addr("192.168.1.99"); 
       
     int n, len; 
+
+    struct sensor_data_t *data = create_sensor_payload(
+        123456, 123456, 123456, 123456, 123456, 123456, 123456
+    );
+
+
+    uint8_t *payload = malloc(SIZE);
+
+    sensor_payload_pack(payload, data); // pack into bytes
+    
+    uint8_t *mainPayload = malloc(SIZE);
+    struct HAB_payload_t *HAB_data = HAB_payload_create(SENSOR_PAYLOAD, payload);
+    HAB_payload_pack(mainPayload, HAB_data);
       
     while(1) {
-        sendto(sockfd, (const char *)hello, strlen(hello), 
-            0x800, (const struct sockaddr *) &servaddr,  
-                sizeof(servaddr)); 
-        printf("Hello message sent.\n"); 
+        printf("%u\n",sendto(sockfd, mainPayload, SIZE, 
+            MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                sizeof(servaddr))); 
+        printf("Sensor data sent.\n"); 
 
         sleep(2);
     }  
