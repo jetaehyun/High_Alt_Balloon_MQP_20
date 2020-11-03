255da55a0738ca0f13a08360486192b4eff7ee29
jetaehyun <jetaehyun0@gmail.com>
2020-11-03 17:25:55 -0500

    added code to read from serial port on rasp pi

--- HAB_Payload/src/main.c
+++ HAB_Payload/src/main.c
@@ -7,62 +7,92 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h>
 #include <netdb.h>
+#include <errno.h>
+#include <termios.h>
+#include <fcntl.h>
 
 #include "Data_Packet/data_packet.h"
 #include "Data_Packet/release_payload.h"
 #include "Data_Packet/sensor_payload.h"
 #include "Database/database.h"
 
-// #include <wiringPi.h>
-
 #define PORT 8808
 #define SIZE 30
 
+// will need to compile with: sudo ./gcs.exe
+
 int main(int argc, const char *argv[]) {
-    int sockfd; 
 
-    struct sockaddr_in     servaddr; 
-  
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-  
-    memset(&servaddr, 0, sizeof(servaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = inet_addr("192.168.1.99"); 
-      
-    int n, len; 
+    int fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_SYNC);
 
-    struct sensor_data_t *data = create_sensor_payload(
-        123456, 123456, 123456, 123456, 123456, 123456, 123456
-    );
+    if(fd < 0) {
+        fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+        return 0;
+    }
 
+    uint8_t *incoming_sensor_data = malloc(sizeof(uint8_t) * SIZE);
+    uint8_t *check_sensor_data = malloc(sizeof(uint8_t) * 28);
+    struct HAB_payload_t *habData = malloc(sizeof(struct HAB_payload_t));
 
-    uint8_t *payload = malloc(SIZE);
-
-    sensor_payload_pack(payload, data); // pack into bytes
-    
-    uint8_t *mainPayload = malloc(SIZE);
-    struct HAB_payload_t *HAB_data = HAB_payload_create(SENSOR_PAYLOAD, payload);
-    HAB_payload_pack(mainPayload, HAB_data);
-      
     while(1) {
-        printf("%u\n",sendto(sockfd, mainPayload, SIZE, 
-            MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
-                sizeof(servaddr))); 
-        printf("Sensor data sent.\n"); 
+        size_t data = read(fd, incoming_sensor_data, SIZE);
+        if(data >= SIZE) {
+            if(HAB_payload_unpack(incoming_sensor_data, habData)) {
+                printf("Data is good\n");
 
+            }
+
+        }
         sleep(2);
-    }  
+    }
+
+
+
+    // int sockfd; 
+
+    // struct sockaddr_in     servaddr; 
+  
+    // // Creating socket file descriptor 
+    // if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+    //     perror("socket creation failed"); 
+    //     exit(EXIT_FAILURE); 
+    // } 
+  
+    // memset(&servaddr, 0, sizeof(servaddr)); 
+      
+    // // Filling server information 
+    // servaddr.sin_family = AF_INET; 
+    // servaddr.sin_port = htons(PORT); 
+    // servaddr.sin_addr.s_addr = inet_addr("192.168.1.99"); 
+      
+    // int n, len; 
+
+    // struct sensor_data_t *data = create_sensor_payload(
+    //     123456, 123456, 123456, 123456, 123456, 123456, 123456
+    // );
+
+
+    // uint8_t *payload = malloc(SIZE);
+
+    // sensor_payload_pack(payload, data); // pack into bytes
+    
+    // uint8_t *mainPayload = malloc(SIZE);
+    // struct HAB_payload_t *HAB_data = HAB_payload_create(SENSOR_PAYLOAD, payload);
+    // HAB_payload_pack(mainPayload, HAB_data);
+      
+    // while(1) {
+    //     printf("%u\n",sendto(sockfd, mainPayload, SIZE, 
+    //         MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+    //             sizeof(servaddr))); 
+    //     printf("Sensor data sent.\n"); 
+
+    //     sleep(2);
+    // }  
 
   
-    close(sockfd); 
-
+    // close(sockfd); 
+    close(fd);
+    free(incoming_sensor_data);
            
 
     return 0;
