2c68c95d75d6fd7a7586eb7ac03b41e16d2708e1
jetaehyun <jetaehyun0@gmail.com>
2020-11-04 21:38:43 -0500

    seperated udp code

--- HAB_Payload/src/main.c
+++ HAB_Payload/src/main.c
@@ -15,42 +15,18 @@
 #include "Data_Packet/release_payload.h"
 #include "Data_Packet/sensor_payload.h"
 #include "Database/database.h"
+#include "udp.h"
 
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
 #define PORT 8808
 #define SIZE 30
+#define MAX_ALTITUDE 90000.0
 
 // will need to compile with: sudo ./gcs.exe
 
 int main(int argc, const char *argv[]) {
-
-    // int fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_SYNC);
-
-    // if(fd < 0) {
-    //     fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
-    //     return 0;
-    // }
-
-    // uint8_t *incoming_sensor_data = malloc(sizeof(uint8_t) * SIZE);
-    // uint8_t *check_sensor_data = malloc(sizeof(uint8_t) * 28);
-    // struct HAB_payload_t *habData = malloc(sizeof(struct HAB_payload_t));
-
-    // while(1) {
-    //     size_t data = read(fd, incoming_sensor_data, SIZE);
-    //     printf("%d\n", data);
-
-    //     // if(data >= SIZE) {
-    //     HAB_payload_unpack(incoming_sensor_data, habData);
-    //     printf("TYPE: %d\n", habData->payload_type); 
-    //     struct sensor_data_t w = sensor_payload_unpack(habData->payload);
-    //     printf("%d, %d, %d, %d, %d, %d, %d\n", w.altitude, w.CO2_sensor, w.NO2_sensor, w.Ozone_sensor, w.pressure_sensor, w.temp_sensor, w.UV_sensor);
-            
-
-    //     // }
-    //     sleep(2);
-    // }
 
     int fd = serialOpen("/dev/ttyS0", 9600);
 
@@ -59,26 +35,32 @@ int main(int argc, const char *argv[]) {
         return 0;
     }
 
-    uint8_t *incoming_sensor_data = malloc(sizeof(uint8_t) * SIZE);
-    uint8_t *check_sensor_data = malloc(sizeof(uint8_t) * 28);
+    uint8_t *incoming_sensor_data = malloc(SIZE);
+    // uint8_t *check_sensor_data = malloc(sizeof(uint8_t) * 28);
     struct HAB_payload_t *habData = malloc(sizeof(struct HAB_payload_t));
 
     while(1) {
+
         int data = serialDataAvail(fd);
         printf("%d\n", data);
 
         if(data >= SIZE) {
-        for(int i = 0; i < SIZE; i++) {
-            incoming_sensor_data[i] = (uint8_t)serialGetchar(fd);
-        }
-        HAB_payload_unpack(incoming_sensor_data, habData);
-        printf("TYPE: %d\n", habData->payload_type); 
-        struct sensor_data_t w = sensor_payload_unpack(habData->payload);
-        printf("%d, %d, %d, %d, %d, %d, %d\n", w.altitude, w.CO2_sensor, w.NO2_sensor, w.Ozone_sensor, w.pressure_sensor, w.temp_sensor, w.UV_sensor);
+
+            for(int i = 0; i < SIZE; i++) {
+                incoming_sensor_data[i] = (uint8_t)serialGetchar(fd);
+            }
+
+            HAB_payload_unpack(incoming_sensor_data, habData);
+            // printf("TYPE: %d\n", habData->payload_type); 
+            struct sensor_data_t w = sensor_payload_unpack(habData->payload);
+            printf("%d, %d, %d, %d, %d, %d, %d\n", w.altitude, w.CO2_sensor, w.NO2_sensor, w.Ozone_sensor, w.pressure_sensor, w.temp_sensor, w.UV_sensor);
             
+            if(w.altitude > MAX_ALTITUDE) break; // flight termination
 
         }
+
         sleep(2);
+
     }
 
 
@@ -125,8 +107,9 @@ int main(int argc, const char *argv[]) {
     // }  
 
   
-    // close(sockfd); 
+    // closeServer();
     close(fd);
+    free(habData);
     free(incoming_sensor_data);
            
 
